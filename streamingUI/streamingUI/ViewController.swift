36b13f35d2289d15666cab2c48620d0b0356fb1f
Sataphon Obra <puye53991@gmail.com>
2022-08-14 22:29:36 +0700

    second commit

--- streamingUI/streamingUI/ViewController.swift
+++ streamingUI/streamingUI/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+//i try put comment it to see if i can commit
 
 }
 
